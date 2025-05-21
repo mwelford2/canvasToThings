f57cbdc0a62b689732a1d830fe50a4bc14049b14
Matteo Welford <83231271+mwelford2@users.noreply.github.com>
2025-05-20 21:48:08 -0400

    Update Email.py

--- Email.py
+++ Email.py
@@ -1,9 +1,14 @@
 from main import send_email
 
-with open('weekRuns.txt', 'r') as w:
-    weekRuns = int(w.read())
-with open('weekRuns.txt', 'w') as w:
-    w.write(str(0));
-times = "times"
-if weekRuns == 1: times = "time"
-send_email("Canvas to Things ran this week", f"Canvas to Things ran {weekRuns} times this week", "mwelford2@gmail.com")
+def run_emailer:
+    with open('weekRuns.txt', 'r') as w:
+        weekRuns = int(w.read())
+    with open('weekRuns.txt', 'w') as w:
+        w.write(str(0));
+    times = "times"
+    if weekRuns == 1: times = "time"
+    send_email("Canvas to Things ran this week", f"Canvas to Things ran {weekRuns} times this week", "mwelford2@gmail.com")
+    
+if __name__ == "__main__": 
+    # uncomment this to enable the script
+    # run_emailer 
