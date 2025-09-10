b9e1e2f17bf12ec402bfd759a9529e65ec51f0c4
mwelford2 <83231271+mwelford2@users.noreply.github.com>
2025-09-10 15:35:09 -0400

    fjkeajlfs

--- Email.py
+++ Email.py
@@ -5,10 +5,8 @@ def run_emailer():
         weekRuns = int(w.read())
     with open('weekRuns.txt', 'w') as w:
         w.write(str(0));
-    times = "times"
-    if weekRuns == 1: times = "time"
     send_email("Canvas to Things ran this week", f"Canvas to Things ran {weekRuns} times this week", "mwelford2@gmail.com")
     
 if __name__ == "__main__": 
     # uncomment this to enable the script
-    # run_emailer 
+    run_emailer()
