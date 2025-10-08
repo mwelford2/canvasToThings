bf6df8b39d2da6e30f0cbadfa867e25871455d1c
mwelford2 <83231271+mwelford2@users.noreply.github.com>
2025-10-08 11:04:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def get_assignments():
     for c in classes:
         req = requests.get(api_url+f"courses/{c}/assignments?bucket=future", headers=headers)
         req2 = requests.get(api_url+f"courses/{c}/assignments?bucket=upcoming", headers=headers)
-        cur_ass = json.loads(req.text) + json.loads(req2.text)
+        cur_ass = json.loads(req.text) + json.loads(req2.text) # join upcoming and future assignments in one dict
         for stick in cur_ass:
             # print(stick['name'], stick['due_at'], end='  UPDATED DATE: ')
             if not stick['due_at'] is None:
@@ -70,17 +70,27 @@ def get_assignments():
             # print(ass)
             # print(stick['id'])
             # print(content.find('6268132'))
-            if content.find(str(stick['id'])) == -1:
+            if content.find(str(stick['id'])) == -1: # make sure assignment hasn't been processed before
                 print("adding assignment", stick['name'])
+                if "Homework Board" in str(stick['name']) and c == 540816: # IMPORTANT: SPECIFIC FOR AEB2280, REMOVE IN THE FUTURE
+                    add_reflection_post(class_name, due_at, stick['name'])
                 with open('assignments.txt', 'a') as f:
                     f.write('\n')
                     f.write(str(stick['id']))
                     f.close()
-                send_email(stick['name'], class_name + '\n' + str(due_at) + '\n' + 'Sent from git', THINGS_EMAIL)
-                send_email(f"Assignment: {stick['name']} added", class_name + "\n" + str(due_at) + "\n" + "Sent from mac", "canvastothings@gmail.com")
+                send_email(stick['name'], class_name + '\n' + str(due_at), THINGS_EMAIL)
+                # send_email(f"Assignment: {stick['name']} added", class_name + "\n" + str(due_at) + "\n" + "Sent from mac", "canvastothings@gmail.com")
                 name_added.append(stick['name'])
                 num_added+=1
     return num_added, name_added
+
+
+def add_reflection_post(class_name, due_at, board_name):
+    name = str(board_name).replace('Homework Board', 'Reflection Post')
+    due_date = due_at + timedelta(days=7)
+    send_email(name, class_name + '\n' + str(due_date), THINGS_EMAIL)
+    return name
+
 if __name__ == '__main__':
     num, names = get_assignments()
     n = str(names).strip("[]")
